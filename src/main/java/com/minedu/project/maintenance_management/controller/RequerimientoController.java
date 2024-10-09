803369fe307ca76504917e22527af7f67d0b849b
fabiana <i202315244@cibertec.edu.pe>
2024-10-09 16:56:55 -0500

    eliminacion de un import

--- src/main/java/com/minedu/project/maintenance_management/controller/RequerimientoController.java
+++ src/main/java/com/minedu/project/maintenance_management/controller/RequerimientoController.java
@@ -6,8 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.minedu.project.maintenance_management.model.Requerimiento;
 import com.minedu.project.maintenance_management.service.RequerimientoService;
 import com.minedu.project.maintenance_management.service.SolicitudService;
